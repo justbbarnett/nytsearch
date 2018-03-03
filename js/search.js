26a302f1b3b7efd867f84e1cdcdb38451db2c29c
justbbarnett <justbbarnett@Bs-MacBook-Air.local>
2018-03-03 13:56:44 -0600

    with solved folder

--- js/search.js
+++ js/search.js
@@ -1,10 +1,12 @@
 // NYT Search JS
 
+var q =  $("#searchTerm").val().trim()
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" 
+
+url + "?" + $.q({
   'api-key': "67889b89197849c38d3b8ad358a43165",
-  'q': searchTerm;
+  
 });
 $.ajax({
   url: url,
@@ -19,9 +21,3 @@ $.ajax({
 
 
 
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-<script src="js/search.js"></script>
